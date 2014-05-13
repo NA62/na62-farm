32fc38e5e47d9286aeb1b10cd2a6421fedf27fe9
Jonas Kunze <kunze.jonas@gmail.com>
2014-05-13 14:35:33 +0200

    Moved OPTION_CONFIG_FILE definition from na62-farm-lib to this repository

--- src/options/MyOptions.h
+++ src/options/MyOptions.h
@@ -70,7 +70,9 @@ public:
 	virtual ~MyOptions();
 
 	static void Load(int argc, char* argv[]) {
-		desc.add_options()
+		desc.add_options()(OPTION_CONFIG_FILE,
+				po::value<std::string>()->default_value("/etc/na62-farm.cfg"),
+				"Config file for the options shown here")
 
 		(OPTION_L0_RECEIVER_PORT, po::value<int>()->default_value(58913),
 				"UDP-Port for L1 data reception")
