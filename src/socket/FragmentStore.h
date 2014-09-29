66061c98f6baa32d919b50b52ce0d11bcfb16e99
Jonas Kunze <kunze.jonas@gmail.com>
2014-09-29 18:45:17 +0200

    Implemented FragmentStore storing IP fragments and reassembling them

---
+++ src/socket/FragmentStore.h
@@ -0,0 +1,109 @@
+/*
+ * FragmentStore.h
+ *
+ *  Created on: Sep 29, 2014
+ *      Author: root
+ */
+
+#ifndef FRAGMENTSTORE_H_
+#define FRAGMENTSTORE_H_
+
+#include <socket/EthernetUtils.h>
+#include <structs/Network.h>
+#include <sys/types.h>
+#include <map>
+#include <vector>
+#include <tbb/spin_mutex.h>
+
+#include <algorithm>
+#include <iterator>
+
+namespace na62 {
+
+class FragmentStore {
+
+public:
+	static DataContainer addFragment(DataContainer&& fragment) {
+		tbb::spin_mutex::scoped_lock my_lock(newFragmentMutex_);
+
+		UDP_HDR* hdr = (UDP_HDR*) fragment.data;
+		auto& fragmentVector = fragmentsById_[hdr->ip.id];
+
+		fragmentVector.push_back(std::move(fragment));
+
+		uint sumOfPayloadBytes = 0;
+		UDP_HDR* lastFragment = nullptr;
+
+		for (auto& frag : fragmentsById_[hdr->ip.id]) {
+			UDP_HDR* hdr = (UDP_HDR*) frag.data;
+
+			sumOfPayloadBytes += ntohs(hdr->ip.tot_len) - sizeof(iphdr);
+
+			if (!hdr->isMoreFragments()) {
+				lastFragment = hdr;
+			}
+		}
+		if (lastFragment != nullptr) {
+			uint expectedPayloadSum = lastFragment->getFragmentOffsetInBytes()
+					+ ntohs(lastFragment->ip.tot_len) - sizeof(iphdr);
+			std::cout << expectedPayloadSum << "\t" << sumOfPayloadBytes
+					<< std::endl;
+			if (expectedPayloadSum == sumOfPayloadBytes) {
+				return reassembleFrame(fragmentVector);
+			}
+		}
+
+		return {nullptr, 0, false};
+	}
+
+private:
+	static std::map<ushort, std::vector<DataContainer>> fragmentsById_;
+	static tbb::spin_mutex newFragmentMutex_;
+
+	static DataContainer reassembleFrame(std::vector<DataContainer> fragments) {
+		/*
+		 * Sort the fragments by offset
+		 */
+		std::sort(fragments.begin(), fragments.end(),
+				[](const DataContainer& a, const DataContainer& b) {
+					UDP_HDR* hdr1 = (UDP_HDR*) a.data;
+					UDP_HDR* hdr2 = (UDP_HDR*) b.data;
+					return hdr1->getFragmentOffsetInBytes() < hdr2->getFragmentOffsetInBytes();
+				});
+
+		UDP_HDR* lastFragment = (UDP_HDR*) fragments.back().data;
+		uint16_t totalBytes = lastFragment->getFragmentOffsetInBytes()
+				+ ntohs(lastFragment->ip.tot_len) - sizeof(iphdr);
+
+		char* newFrameBuff = new char[totalBytes];
+
+		uint16_t currentOffset = 0;
+		for (DataContainer& fragment : fragments) {
+			UDP_HDR* currentData = (UDP_HDR*) fragment.data;
+
+			if (currentData->getFragmentOffsetInBytes() != currentOffset) {
+				std::cerr
+						<< "Error while reassembling IP fragments: sum of fragment lengths is "
+						<< currentOffset << " but offset of current frame is "
+						<< currentData->getFragmentOffsetInBytes() << std::endl;
+
+				exit(1);
+			}
+
+			/*
+			 * Copy the payload of the IP datagram to the buffer
+			 *
+			 * The payload starts after sizeof(ether_header) + sizeof(iphdr) and is ntohs(currentData->ip.tot_len) - sizeof(iphdr) bytes long
+			 */
+			memcpy(newFrameBuff + currentOffset,
+					fragment.data + sizeof(ether_header) + sizeof(iphdr),
+					ntohs(currentData->ip.tot_len) - sizeof(iphdr));
+			currentOffset += ntohs(currentData->ip.tot_len) - sizeof(iphdr);
+		}
+		return {newFrameBuff, currentOffset, true};
+	}
+};
+
+} /* namespace na62 */
+
+#endif /* FRAGMENTSTORE_H_ */
--- src/socket/HandleFrameTask.cpp
+++ src/socket/HandleFrameTask.cpp
@@ -36,6 +36,7 @@
 #include "../eventBuilding/L2Builder.h"
 #include "../options/MyOptions.h"
 #include "PacketHandler.h"
+#include "FragmentStore.h"
 
 namespace na62 {
 
@@ -119,6 +120,11 @@ tbb::task* HandleFrameTask::execute() {
 			return nullptr;
 		}
 
+		if(hdr->isFragment()){
+			FragmentStore::addFragment(std::move(container));
+			return nullptr;
+		}
+
 		const char * UDPPayload = container.data + sizeof(struct UDP_HDR);
 		const uint16_t & dataLength = ntohs(hdr->udp.len)
 				- sizeof(struct udphdr);
@@ -140,7 +146,7 @@ tbb::task* HandleFrameTask::execute() {
 			if (nextBurstID_ != currentBurstID_
 					&& mep->getFirstEventNum() < 1000
 					&& eobFrameReceivedTime_.elapsed().wall / 1E6
-							> 100 /*100ms*/) {
+							> 1000 /*1s*/) {
 				currentBurstID_ = nextBurstID_;
 			}
 
@@ -213,6 +219,11 @@ bool HandleFrameTask::checkFrame(struct UDP_HDR* hdr, uint16_t length) {
 	//					delete[] container.data;
 	//					continue;
 	//				}
+
+	if(hdr->isFragment()){
+		return true;
+	}
+
 	if (ntohs(hdr->ip.tot_len) + sizeof(ether_header) != length) {
 		/*
 		 * Does not need to be equal because of ethernet padding
