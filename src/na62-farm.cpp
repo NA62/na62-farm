7b2962c77cc9131c5fa5b1f2245905d376a7610d
Angela Romano <Angela.Romano@cern.ch>
2016-04-25 21:09:41 +0200

    - Indentation, plus add xml file reading for HLT configuration paramenter

--- src/na62-farm.cpp
+++ src/na62-farm.cpp
@@ -11,7 +11,6 @@
 #include <thread>
 #include <atomic>
 
-
 #include <monitoring/IPCHandler.h>
 #include <monitoring/BurstIdHandler.h>
 #include <monitoring/FarmStatistics.h>
@@ -23,6 +22,8 @@
 #include <vector>
 #include <l1/L1TriggerProcessor.h>
 #include <l2/L2TriggerProcessor.h>
+#include <common/HLTriggerManager.h>
+#include <struct/HLTConfParams.h>
 #include <eventBuilding/EventPool.h>
 #include <eventBuilding/Event.h>
 #include <l1/L1DistributionHandler.h>
@@ -40,7 +41,6 @@
 #include "socket/HandleFrameTask.h"
 #include "monitoring/CommandConnector.h"
 //#include "straws/StrawReceiver.h"
-
 
 using namespace std;
 using namespace na62;
@@ -92,31 +92,32 @@ void handle_stop(const boost::system::error_code& error, int signal_number) {
 }
 
 void onBurstFinished() {
-    static std::atomic<uint> incompleteEvents_;
-    incompleteEvents_ = 0;
-
+	static std::atomic<uint> incompleteEvents_;
+	incompleteEvents_ = 0;
 
 #ifdef HAVE_TCMALLOC
-    // Do it with parallel_for using tbb if tcmalloc is linked
+	// Do it with parallel_for using tbb if tcmalloc is linked
 	tbb::parallel_for(
-			tbb::blocked_range<uint_fast32_t>(0, EventPool::getLargestTouchedEventnumberIndex() + 1,
-					EventPool::getLargestTouchedEventnumberIndex() / std::thread::hardware_concurrency()),
-					[](const tbb::blocked_range<uint_fast32_t>& r) {
-		for(size_t index=r.begin();index!=r.end(); index++) {
-			Event* event = EventPool::getEventByIndex(index);
-			if(event == nullptr) continue;
-			if (event->isUnfinished()) {
-					//LOG_ERROR << "Incomplete event " << (uint)(event->getEventNumber());
-				if(event->isLastEventOfBurst()) {
-					LOG_ERROR << "type = EOB : Handling unfinished EOB event " << event->getEventNumber()<< ENDL;
-					StorageHandler::SendEvent(event);
+			tbb::blocked_range<uint_fast32_t>(0,
+					EventPool::getLargestTouchedEventnumberIndex() + 1,
+					EventPool::getLargestTouchedEventnumberIndex()
+							/ std::thread::hardware_concurrency()),
+			[](const tbb::blocked_range<uint_fast32_t>& r) {
+				for(size_t index=r.begin();index!=r.end(); index++) {
+					Event* event = EventPool::getEventByIndex(index);
+					if(event == nullptr) continue;
+					if (event->isUnfinished()) {
+						//LOG_ERROR << "Incomplete event " << (uint)(event->getEventNumber());
+						if(event->isLastEventOfBurst()) {
+							LOG_ERROR << "type = EOB : Handling unfinished EOB event " << event->getEventNumber()<< ENDL;
+							StorageHandler::SendEvent(event);
+						}
+						++incompleteEvents_;
+						event->updateMissingEventsStats();
+						EventPool::freeEvent(event);
+					}
 				}
-				++incompleteEvents_;
-				event->updateMissingEventsStats();
-				EventPool::freeEvent(event);
-			}
-		}
-	});
+			});
 #else
 	for (uint idx = 0; idx != EventPool::getLargestTouchedEventnumberIndex() + 1; ++idx) {
 		Event* event = EventPool::getEventByIndex(idx);
@@ -128,8 +129,8 @@ void onBurstFinished() {
 	}
 #endif
 
-	if(incompleteEvents_ > 0) {
-		LOG_ERROR << "type = EOB : Dropped " << incompleteEvents_ << " events in burst ID = " << (int) BurstIdHandler::getCurrentBurstId() << ".";
+	if (incompleteEvents_ > 0) {
+		LOG_ERROR<< "type = EOB : Dropped " << incompleteEvents_ << " events in burst ID = " << (int) BurstIdHandler::getCurrentBurstId() << ".";
 	}
 }
 
@@ -143,7 +144,6 @@ int main(int argc, char* argv[]) {
 	boost::thread signalThread(
 			boost::bind(&boost::asio::io_service::run, &signalService));
 
-
 	L1TriggerProcessor::registerDownscalingAlgorithms();
 
 	L1TriggerProcessor::registerReductionAlgorithms();
@@ -155,12 +155,17 @@ int main(int argc, char* argv[]) {
 
 	ZMQHandler::Initialize(Options::GetInt(OPTION_ZMQ_IO_THREADS));
 
+	HLTStruct HLTConfParams;
+	HLTriggerManager::fillStructFromXMLFile(HLTConfParams);
+	L1TriggerProcessor::initialize(HLTConfParams.l1);
+	L2TriggerProcessor::initialize(HLTConfParams.l2);
+
 //	L1TriggerProcessor::initialize(
 //			TriggerOptions::GetDouble(OPTION_L1_BYPASS_PROBABILITY));
 //	L2TriggerProcessor::initialize(
 //			TriggerOptions::GetDouble(OPTION_L2_BYPASS_PROBABILITY));
-	L1TriggerProcessor::initialize();
-	L2TriggerProcessor::initialize();
+//	L1TriggerProcessor::initialize();
+//	L2TriggerProcessor::initialize();
 
 	FarmStatistics::init();
 	FarmStatistics farmstats;
@@ -175,7 +180,8 @@ int main(int argc, char* argv[]) {
 			Options::GetIntPairList(OPTION_DATA_SOURCE_IDS),
 			Options::GetIntPairList(OPTION_L1_DATA_SOURCE_IDS));
 
-	BurstIdHandler::initialize(Options::GetInt(OPTION_FIRST_BURST_ID), &onBurstFinished);
+	BurstIdHandler::initialize(Options::GetInt(OPTION_FIRST_BURST_ID),
+			&onBurstFinished);
 
 	HandleFrameTask::initialize();
 
@@ -188,22 +194,27 @@ int main(int argc, char* argv[]) {
 
 	Event::initialize(MyOptions::GetBool(OPTION_PRINT_MISSING_SOURCES));
 
-    // Get the list of farm nodes and find my position
-    vector<std::string> nodes = Options::GetStringList(OPTION_FARM_HOST_NAMES);
-    std::string myIP = EthernetUtils::ipToString(EthernetUtils::GetIPOfInterface(Options::GetString(OPTION_ETH_DEVICE_NAME)));
-    uint logicalNodeID = 0xffffffff;
-    for (size_t i=0; i< nodes.size(); ++i) {
-            if (myIP==nodes[i]) {
-                    logicalNodeID = i;
-                    break;
-            }
-    }
-    if (logicalNodeID == 0xffffffff) {
-            LOG_ERROR << "You must provide a list of farm nodes IP addresses containing the IP address of this node!";
-            exit(1);
-    }
+	// Get the list of farm nodes and find my position
+	vector<std::string> nodes = Options::GetStringList(OPTION_FARM_HOST_NAMES);
+	std::string myIP = EthernetUtils::ipToString(
+			EthernetUtils::GetIPOfInterface(
+					Options::GetString(OPTION_ETH_DEVICE_NAME)));
+	uint logicalNodeID = 0xffffffff;
+	for (size_t i = 0; i < nodes.size(); ++i) {
+		if (myIP == nodes[i]) {
+			logicalNodeID = i;
+			break;
+		}
+	}
+	if (logicalNodeID == 0xffffffff) {
+		LOG_ERROR<< "You must provide a list of farm nodes IP addresses containing the IP address of this node!";
+		exit(1);
+	}
 
-    EventPool::initialize(Options::GetInt(OPTION_MAX_NUMBER_OF_EVENTS_PER_BURST), nodes.size(), logicalNodeID, Options::GetInt(OPTION_NUMBER_OF_FRAGS_PER_L0MEP));
+	EventPool::initialize(
+			Options::GetInt(OPTION_MAX_NUMBER_OF_EVENTS_PER_BURST),
+			nodes.size(), logicalNodeID,
+			Options::GetInt(OPTION_NUMBER_OF_FRAGS_PER_L0MEP));
 
 	l1::L1DistributionHandler::Initialize(
 			Options::GetInt(OPTION_MAX_TRIGGERS_PER_L1MRP),
@@ -215,7 +226,7 @@ int main(int argc, char* argv[]) {
 	/*
 	 * Burst Handler
 	 */
-	LOG_INFO << "Start burst handler thread.";
+	LOG_INFO<< "Start burst handler thread.";
 	BurstIdHandler bHandler;
 	bHandler.startThread("BurstHandler");
 	/*
@@ -243,7 +254,8 @@ int main(int argc, char* argv[]) {
 		PacketHandler* handler = new PacketHandler(i);
 		packetHandlers.push_back(handler);
 
-		uint coresPerSocket = std::thread::hardware_concurrency() / 2/*hyperthreading*/;
+		uint coresPerSocket = std::thread::hardware_concurrency()
+				/ 2/*hyperthreading*/;
 		uint cpuMask = i % 2 == 0 ? i / 2 : coresPerSocket + i / 2;
 		handler->startThread(i, "PacketHandler", cpuMask, 25,
 				MyOptions::GetInt(OPTION_PH_SCHEDULER));
@@ -251,7 +263,9 @@ int main(int argc, char* argv[]) {
 	}
 
 	//for (unsigned int i = 0; i < 4; i++) {
-	for (unsigned int i = 0; i < std::thread::hardware_concurrency() - numberOfPacketHandler; i++) {
+	for (unsigned int i = 0;
+			i < std::thread::hardware_concurrency() - numberOfPacketHandler;
+			i++) {
 		TaskProcessor* tp = new TaskProcessor();
 		taskProcessors.push_back(tp);
 		tp->startThread(i, "TaskProcessor");
